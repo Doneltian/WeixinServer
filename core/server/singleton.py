50c92ef1f9d0d0ca98e3b019fb468730da050936
tianjia <1207939366@qq.com>
2018-02-12 17:50:17 +0800

    m

--- core/server/singleton.py
+++ core/server/singleton.py
@@ -1,5 +1,5 @@
 class Singleton(object):
     def __new__(cls, *args, **kwargs):
         if not hasattr(cls,'__instance'):
-            cls.__instance = super().__new__()
+            cls.__instance = super().__new__(cls,*args, **kwargs)
         return cls.__instance
