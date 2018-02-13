604564d44ec3192cd3343ff4666860f9696631a8
tianjia <1207939366@qq.com>
2018-02-13 11:03:18 +0800

    m

--- core/server/page_handler.py
+++ core/server/page_handler.py
@@ -1,13 +1,18 @@
 import tornado.web
 
+from core.logger_helper import logger
+
+
 class PageHandler(tornado.web.RequestHandler):
     """
     微信handler处理类
     """
     def post(self, flag):
+        logger.debug('微信handler处理类】<<<<<<<<<' + flag)
         if flag == 'index':
             """首页"""
             self.render('index')
 
     def get(self):
-        self.render('index.html')
+        logger.debug('微信handler处理类】》》》》》》》》》》')
+        self.render('index')
