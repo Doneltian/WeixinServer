6d99b60b5b45c1179de064ffb8094d5c4bc5e459
tianjia <1207939366@qq.com>
2018-02-13 10:03:24 +0800

    m

--- core/server/wx_handler.py
+++ core/server/wx_handler.py
@@ -20,27 +20,25 @@ class WxHandler(tornado.web.RequestHandler):
     '''redis服务'''
     wx_token_cache = TokenCache()
 
-    def get(self, flag):
-        logger.debug('微信网页授权】flag>>>>>>>' + str(flag))
-        if flag == 'wxauthor':
-            '''微信网页授权'''
-            body = self.request.body
-            logger.debug('微信网页授权】收到消息' + str(body.decode('utf-8')))
+    def get(self):
+        '''微信网页授权'''
+        body = self.request.body
+        logger.debug('微信网页授权】收到消息' + str(body.decode('utf-8')))
 
-            code = self.get_argument('code')
-            state = self.get_argument('state')
-            # 获取重定向的url
-            redirect_url = self.wx_config.wx_menu_state_map[state]
-            logger.debug('【微信网页授权】将要重定向的地址为:redirct_url[' + redirect_url + ']')
-            logger.debug('【微信网页授权】用户同意授权，获取code>>>>code[' + code + ']state[' + state + ']')
-            if code:
-                # 通过code换取网页授权acccess_token
-                data = self.wx_author_server.get_auth_access_token(code)
-                openid = data['openid']
-                logger.debug('【微信网页授权】openid>>>>openid[' + openid + ']')
-                if openid:
-                    # 跳到自己的业务界面
-                    self.redirect(redirect_url)
-                else:
-                    # 获取不到openid
-                    logger.debug('获取不到openid')
+        code = self.get_argument('code')
+        state = self.get_argument('state')
+        # 获取重定向的url
+        redirect_url = self.wx_config.wx_menu_state_map[state]
+        logger.debug('【微信网页授权】将要重定向的地址为:redirct_url[' + redirect_url + ']')
+        logger.debug('【微信网页授权】用户同意授权，获取code>>>>code[' + code + ']state[' + state + ']')
+        if code:
+            # 通过code换取网页授权acccess_token
+            data = self.wx_author_server.get_auth_access_token(code)
+            openid = data['openid']
+            logger.debug('【微信网页授权】openid>>>>openid[' + openid + ']')
+            if openid:
+                # 跳到自己的业务界面
+                self.redirect(redirect_url)
+            else:
+                # 获取不到openid
+                logger.debug('获取不到openid')
